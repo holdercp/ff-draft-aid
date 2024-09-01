57d99f5cfca0edec28cb8ac5788a1d770d4810a6
Chris Holder <chris@chrisholder.dev>
2024-09-01 17:03:11 -0500

    List available players

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,48 @@
 import "./App.css";
+import rankings from "./data/rankings.json";
+
+type Ranking = {
+  Overall: number;
+  Player: string;
+  Position: Postion;
+  "Pos Rank": number;
+  Tier: number;
+  "Auction (Out of $200)": string;
+};
+
+type Postion = "QB" | "RB" | "WR" | "TE";
+
+type Player = {
+  overallRank: number;
+  name: string;
+  position: Postion;
+  positionRank: number;
+  tier: number;
+};
+
+function mapRankingsToPlayers(rankings: Ranking[]): Player[] {
+  return rankings.map((ranking) => ({
+    overallRank: ranking.Overall,
+    name: ranking.Player,
+    position: ranking.Position,
+    positionRank: ranking["Pos Rank"],
+    tier: ranking.Tier,
+  }));
+}
 
 function App() {
+  const players = mapRankingsToPlayers(rankings as Ranking[]);
   return (
     <main>
       <h1>FF Draft Aid</h1>
+      <section>
+        <h2>Available</h2>
+        <ol>
+          {players.map((player) => (
+            <li key={player.name}>{player.name}</li>
+          ))}
+        </ol>
+      </section>
     </main>
   );
 }
